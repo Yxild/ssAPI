512e5ab258bf37e1f6d40512911908d09122ce09
Yxild <che.barton03@gmail.com>
2024-03-11 21:09:37 +1100

    Update app.js
    Fixed the proper handles, added Helmet back & encode each script for easier portability :)

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require('express'); // Web Handling
+const helmet = require('helmet'); // Web Protection
+
 const app = express();
 const port = 1234;
 
 app.use(helmet()); // This directly adds protection between the server and some faggot
 app.use(express.json());
+app.use(express.static('public'))
 const servers = {};
 
 // Connects a server
 app.post('/connect', (req, res) => {
     const { gameId, jobId } = req.body;
     if (!servers[gameId])
-        servers[gameId] = {  };
+        servers[gameId] = {};
 
-    servers[gameId][jobId] = {  };
+    servers[gameId][jobId] = {};
 
     console.log(`${gameId} @ ${jobId} : Connected Server`);
     res.json({ success: true });
@@ -24,10 +26,7 @@ app.post('/disconnect', (req, res) => {
     const { gameId, jobId } = req.body;
     delete servers[gameId][jobId];
 
-    if (servers[gameId][1] == null)
-        delete servers[gameId] // Saves space for the table
-
-        console.log(`${gameId} @ ${jobId} : Disconnected server`);
+    console.log(`${gameId} @ ${jobId} : Disconnected server`);
     res.json({ success: true });
 });
 
@@ -40,10 +39,10 @@ app.get('/servers', (req, res) => {
 app.post('/runscript/', (req, res) => {
     try {
         const { gameId, jobId, script } = req.body;
-        console.log(`${gameId} @ ${jobId} : Added script to script queue...`);
+        console.log(`${gameId} @ ${jobId} : Added script to script queue, encoded: ${Buffer.from(script).toString('base64')}`);
 
         if (servers[gameId][jobId]) {
-            servers[gameId][jobId].script = script;
+            servers[gameId][jobId].script = Buffer.from(script).toString('base64');
             res.json({ success: true });
         } else {
             res.status(404).json({ error: 'Server not found' });
@@ -56,15 +55,23 @@ app.post('/runscript/', (req, res) => {
 
 // Backend check for the Script Queue
 app.get('/check/:gameId/:jobId', (req, res) => {
-    const { gameId, jobId } = req.params;
+    try {
+        const { gameId, jobId } = req.params;
+        const script = servers[gameId][jobId]?.script;
 
-    const script = servers[gameId][jobId]?.script;
-
-    if (script) {
-        res.json({ success: true, script });
-        delete servers[gameId][jobId].script;
-    } else {
-        res.json({ success: false, message: "No script available for this server." });
+        if (script) {
+            if (servers[gameId][jobId]) {
+                res.json({ success: true, script });
+                delete servers[gameId][jobId].script;
+            } else {
+                res.json({ success: false, message: 'Server not found' });
+            }
+        } else {
+            res.json({ success: false, message: 'Script not found' });
+        }
+    } catch (error) {
+        console.error('Error parsing JSON data:', error);
+        res.status(400).json({ success: false, error: 'Invalid JSON data' });
     }
 });
 
