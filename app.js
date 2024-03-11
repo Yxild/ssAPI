d17276e57dd1c2440b79bd67d40311b3b27d0051
Yxild <che.barton03@gmail.com>
2024-03-11 17:16:31 +1100

    Update app.js
    Fixed the console, added a check each time the server/game gets added & also a check when a script gets executed.

--- app.js
+++ app.js
@@ -5,43 +5,45 @@ const port = 1234;
 
 app.use(helmet()); // This directly adds protection between the server and some faggot
 app.use(express.json());
-const Servers = {};
-
-app.use((req, res, next) => {
-    console.log(`Incoming ${req.method} request to ${req.originalUrl}:`, req.body);
-    next();
-});
+const servers = {};
 
 // Connects a server
 app.post('/connect', (req, res) => {
-    const { id } = req.body;
-    Servers[id] = {};
+    const { gameId, jobId } = req.body;
+    if (!servers[gameId])
+        servers[gameId] = {  };
 
+    servers[gameId][jobId] = {  };
+
+    console.log(`${gameId} @ ${jobId} : Connected Server`);
     res.json({ success: true });
 });
 
 // Disconnects a server
 app.post('/disconnect', (req, res) => {
-    const { id } = req.body;
-    console.log(`Disconnected server: ${id}`);
-    delete Servers[id];
+    const { gameId, jobId } = req.body;
+    delete servers[gameId][jobId];
 
+    if (servers[gameId][1] == null)
+        delete servers[gameId] // Saves space for the table
+
+        console.log(`${gameId} @ ${jobId} : Disconnected server`);
     res.json({ success: true });
 });
 
 // Returns the table of avaliable servers
 app.get('/servers', (req, res) => {
-    res.json(Servers);
+    res.json(servers);
 });
 
 // Adds a script to the Script Queue
 app.post('/runscript/', (req, res) => {
     try {
-        const { serverId, script } = req.body;
+        const { gameId, jobId, script } = req.body;
+        console.log(`${gameId} @ ${jobId} : Added script to script queue...`);
 
-        if (Servers[serverId]) {
-            console.log(`Running script on server ${serverId}:`, script);
-            Servers[serverId].script = script;
+        if (servers[gameId][jobId]) {
+            servers[gameId][jobId].script = script;
             res.json({ success: true });
         } else {
             res.status(404).json({ error: 'Server not found' });
@@ -53,16 +55,14 @@ app.post('/runscript/', (req, res) => {
 });
 
 // Backend check for the Script Queue
-app.get('/check/:serverId', (req, res) => {
-    const { serverId } = req.params;
+app.get('/check/:gameId/:jobId', (req, res) => {
+    const { gameId, jobId } = req.params;
 
-    const script = Servers[serverId]?.script;
-    console.log(script);
-    console.log(Servers[serverId]?.script);
+    const script = servers[gameId][jobId]?.script;
 
     if (script) {
         res.json({ success: true, script });
-        delete Servers[serverId].script;
+        delete servers[gameId][jobId].script;
     } else {
         res.json({ success: false, message: "No script available for this server." });
     }
