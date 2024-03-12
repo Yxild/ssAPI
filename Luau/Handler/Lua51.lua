f54dab114f18bdc49c2a82574eb571e67649b381
Yxild <che.barton03@gmail.com>
2024-03-12 14:35:15 +1100

    Create Lua51.lua

---
+++ Luau/Handler/Lua51.lua
@@ -0,0 +1,22 @@
+--[[
+	vLua 51
+]]
+
+local compile = require(script:WaitForChild("Yueliang"))
+local createExecutable = require(script:WaitForChild("FiOne"))
+getfenv().script = nil
+
+return function(source, env)
+	local executable
+	local env = env or getfenv(2)
+	local name = (env.script and env.script:GetFullName())
+	local ran, failureReason = pcall(function()
+		local compiledBytecode = compile(source, name)
+		executable = createExecutable(compiledBytecode, env)
+	end)
+	
+	if ran then
+		return setfenv(executable, env)
+	end
+	return nil, failureReason
+end
